81a4edd01a537b9190c734951312ac20e6bc9735
Dj <ritchey0713@gmail.com>
2019-07-03 16:30:53 -0400

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,17 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root :to => 'users#landing_page'
+
+  get "errors", :to => "users#error_page"
+
+
+  resources :categories, only: [:index, :new, :create, :edit, :update, :show, :destroy]  
+    
+
+  resources :companies, only: [:index, :new, :create, :edit, :update, :show, :destroy] do 
+    resources :jobs, only: [:index, :new, :create, :edit, :update, :show, :destroy]
+  end 
+  
 end
