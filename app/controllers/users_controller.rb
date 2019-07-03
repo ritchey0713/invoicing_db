154839a9d9c98103c806567b822f28d8a9be4cf6
Dj <ritchey0713@gmail.com>
2019-07-03 16:30:47 -0400

    add root and error actions

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,11 @@
+class UsersController < ApplicationController
+    def landing_page 
+    #   if current_user.role != "admin"
+    #     redirect_to "/#{current_user.role}s/new"
+    #   end
+    end
+  
+    def error_page 
+  
+    end
+  end 
