e812eee6d3d63a78d45c1684ae089c0e2cbbd962
Dj <ritchey0713@gmail.com>
2019-07-04 15:17:40 -0400

    fix devise confimable

--- db/migrate/20190703191853_devise_create_users.rb
+++ db/migrate/20190703191853_devise_create_users.rb
@@ -42,9 +42,9 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
       t.integer :hourly_rate
     end
 
-    add_index :users, :email,                unique: true
+    # add_index :users, :email,                unique: true
     add_index :users, :reset_password_token, unique: true
     add_index :users, :confirmation_token,   unique: true
-    add_index :users, :unlock_token,         unique: true
+    # add_index :users, :unlock_token,         unique: true
   end
 end
