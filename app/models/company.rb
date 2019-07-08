1ed81aee2fb2c63521953ba3f2db6da99bd88a9d
Dj <ritchey0713@gmail.com>
2019-07-08 13:30:50 -0400

    change to has many

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,7 +1,7 @@
 class Company < ApplicationRecord
 
     has_and_belongs_to_many :users 
-    has_many :jobs, through: :users
+    has_many :jobs
 
   end
   
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
 
   has_and_belongs_to_many :companies 
-  has_many :jobs, through: :companies 
+  has_many :jobs
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
