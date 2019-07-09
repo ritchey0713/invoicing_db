fa034d863e488142e76b5919a21ed0a3e89b443e
Dj <ritchey0713@gmail.com>
2019-07-09 12:24:22 -0400

    add custom writer

--- app/models/job.rb
+++ app/models/job.rb
@@ -2,5 +2,12 @@ class Job < ApplicationRecord
     belongs_to :user
     belongs_to :company
     belongs_to :category
+
+    # accepts_nested_attributes_for :category 
+
+    def category_attributes=(category_attributes)
+      self.category = Category.where(:name => category_attributes[:name]).first_or_create do |cat|
+        cat.work_type = category_attributes[:work_type]
+      end 
+    end 
   end
-  
